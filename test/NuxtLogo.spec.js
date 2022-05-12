701a28849ec387b2e6cef744e4248746c7e5040a
ASoldo <soldo.andrej@gmail.com>
2022-05-12 20:18:29 +0200

    Update for tests

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -5,5 +5,14 @@ describe('NuxtLogo', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(NuxtLogo)
     expect(wrapper.vm).toBeTruthy()
+
+
   })
+  test('is H1 soldo present', () => {
+    //test to see if component has h1 tag
+    const wrapper = mount(NuxtLogo)
+    const h1 = wrapper.find('h1')
+    expect(h1.exists()).toBeTruthy()
+  })
+
 })
